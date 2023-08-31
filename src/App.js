3ba2545ae9f8bc9417a5030684774c0b291984d9
CodeSandbox <support@codesandbox.io>
2023-08-31 01:11:00 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Resolución tarea GOIT </h1>
+      <hr />
+      <h1>Hello CodeSandbox</h1>
+      <h3>Grupo 1</h3>
+      <p>Navbar</p>
+      <h3>Grupo 2</h3>
+      <p>Instalar react-router-dom</p>
+      <p>Archivo de rutas</p>
+      <p>
+        Home(/), Login(/login), Register(/register),
+        dashboard(/user/:id/gasboard)
+      </p>
+      <h3>Grupo 3</h3>
+      <p>Crear componentes</p>
+      <p>integrar aplicación </p>
+      <p>App.js (BrowserRouter)</p>
+    </div>
+  );
+}
